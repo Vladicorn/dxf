303f3f7ae00dec7ff90331170f9da9addd6d396c
Vladimir aka UnicornOfLove <64689722+Vladicorn@users.noreply.github.com>
2022-09-12 14:20:17 +0300

    Update acdbplaceholder.go

--- object/acdbplaceholder.go
+++ object/acdbplaceholder.go
@@ -1,8 +1,8 @@
 package object
 
 import (
-	"github.com/yofu/dxf/format"
-	"github.com/yofu/dxf/handle"
+	"github.com/Vladicorn/dxf/format"
+	"github.com/Vladicorn/dxf/handle"
 )
 
 // AcDbPlaceHolder represents ACDBPLACEHOLDER Object.
